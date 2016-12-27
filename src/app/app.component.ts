ac67fd175e9e376fc05d4046a9a37712cf083f09
Luis Manuel Cruza <luismanuel.cruza@gmail.com>
2016-12-27 23:52:44 +0100

    Firebase services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+import { FirebaseService, Category, Business } from './services/firebase.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FirebaseService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  businesses: Business[];
+  categories: Category[];
+
+  ngOnInit(){
+    this._firebaseService.getBusinesses().subscribe(businesses => {
+      this.businesses = businesses
+    });
+    this._firebaseService.getCategories().subscribe(categories => {
+      this.categories = categories
+    });
+  }
+
+  constructor(private _firebaseService: FirebaseService) {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AngularFireModule } from 'angularfire2';
+
 import { AppComponent } from './app.component';
+
+
+// Must export the config
+export const firebaseConfig = {
+    apiKey: "AIzaSyCe8y4sexjnRUhFC7Zwz0IIDlTFIZwORLI",
+    authDomain: "businesscontacts-c0362.firebaseapp.com",
+    databaseURL: "https://businesscontacts-c0362.firebaseio.com",
+    storageBucket: "businesscontacts-c0362.appspot.com",
+    messagingSenderId: "903439857675"
+};
 
 @NgModule({
   declarations: [
@@ -11,6 +23,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     FormsModule,
     HttpModule
   ],
